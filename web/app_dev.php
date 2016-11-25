dd2ac2159a99c194f9507285e99ff98ec0fb95c5
NadirZenith <2cb.md2@gmail.com>
2016-11-25 13:08:53 +0100

    remove debug code

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@
 
 require_once __DIR__ . '/../app/bootstrap.php.cache';
 require_once __DIR__ . '/../app/AppKernel.php';
-require_once 'nzdebug.php';
+ini_set('xdebug.max_nesting_level', 500);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
